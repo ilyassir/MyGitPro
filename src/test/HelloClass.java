e6cde73bc41e0aa247d5b0dddf17f83e6cc5d6ff
ilyassir <iliyak6901@brandeis.edu>
2018-03-04 16:57:30 -0500

    this is the newest one

--- src/test/HelloClass.java
+++ src/test/HelloClass.java
@@ -3,6 +3,6 @@ package test;
 public class HelloClass {
 public static void main(String[] args) {
 	System.out.println("Hello class, this is SIR");
-	System.out.println("Hello- Hello");
+	System.out.println("Ok men. That is it for today");
 }
 }
