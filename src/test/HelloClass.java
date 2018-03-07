058b28e71b029e850061c68453e8a8601bea844a
ilyassir <iliyak6901@brandeis.edu>
2018-03-07 12:26:55 -0500

    I added one more sysout line

--- src/test/HelloClass.java
+++ src/test/HelloClass.java
@@ -4,5 +4,6 @@ public class HelloClass {
 public static void main(String[] args) {
 	System.out.println("Hello class, this is SIR");
 	System.out.println("Ok men. That is it for today");
+System.out.println("How to push it?" );
 }
 }
